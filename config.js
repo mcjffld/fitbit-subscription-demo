2ff833a0b8679459ade2e790c20dc7fbb233a645
Michael James <michael.james@priceline.com>
2013-10-28 12:21:28 -0400

    config changes

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
 	production: {
-		db: 'TODO', // ex: mongodb://fitbit-example-user:fitbit-example-user-db-password@mongo.onmodulus.net:27017/1234567
-		fitbitClientKey: 'TODO',
-		fitbitClientSecret: 'TODO',
-		host: 'TODO', // ex: fitbitexample-9501.onmodulus.net
-		twilioAccountSid: 'TODO',
-		twilioAuthToken: 'TODO',
-		twilioPhoneNumber: 'TODO' // ex: +14152363281
+		db: 'mongodb://fb:fb@mongo.onmodulus.net:27017/Imada7me
+		fitbitClientKey: '150da8ff33ad4c7588fc9472d63e43fe',
+		fitbitClientSecret: '9c4e30f313cc4c67b37cf44d0229570b',
+		host: 'fitbitexample-9542.onmodulus.net', // ex: fitbitexample-9501.onmodulus.net
+		twilioAccountSid: 'AC0a917bb6814bd47ffd94144328900ee4',
+		twilioAuthToken: '9da866e1489fde44d81e52c03797ebff',
+		twilioPhoneNumber: '12035027630' // ex: +14152363281
 	}
 };
