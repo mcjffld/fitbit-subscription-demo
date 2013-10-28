52d4a90bbee3da785d7d4839fc0bfd0ac84cb584
Michael James <michael.james@priceline.com>
2013-10-28 12:33:20 -0400

    missed a comma

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	production: {
-		db: 'mongodb://fb:fb@mongo.onmodulus.net:27017/Imada7me'
+		db: 'mongodb://fb:fb@mongo.onmodulus.net:27017/Imada7me',
 		fitbitClientKey: '150da8ff33ad4c7588fc9472d63e43fe',
 		fitbitClientSecret: '9c4e30f313cc4c67b37cf44d0229570b',
 		host: 'fitbitexample-9542.onmodulus.net', // ex: fitbitexample-9501.onmodulus.net
